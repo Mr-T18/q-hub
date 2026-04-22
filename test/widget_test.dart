a22a09f48c2ea71937390541abbb2dc486d6dbab
Mr-T18 <itsuki.tkhs@gmail.com>
2026-04-22 21:56:16 +0900

    createCount patch

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:quiz_hub/main.dart';
+import "package:quiz_hub/ui/screens/main_navigation.dart";
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MainNavigation());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
